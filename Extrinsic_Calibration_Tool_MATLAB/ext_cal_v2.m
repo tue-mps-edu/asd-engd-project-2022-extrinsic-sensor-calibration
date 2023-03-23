41e4d3f493f226dc6fc0ef49ebcbc088c29f792e
adityarajkuk <adityarajkuk@gmail.com>
2023-03-23 22:18:56 +0100

    Update

--- Extrinsic_Calibration_Tool_MATLAB/ext_cal_v2.m
+++ Extrinsic_Calibration_Tool_MATLAB/ext_cal_v2.m
@@ -28,25 +28,11 @@ Yr(3,:) = Yr(3,:);
 
 %% Find the extrinsic parameters of the radar-camera sensor system wrt camera
 
-
-% x0 = [0 0 pi/2 0 0 0];
-% [ext, cost] = Calib_Opt_Par(Yc, Yr, x0, camera_intrinsic);
-% 
-% for i = 1:50
-%     a = (-pi + 2*pi*rand(1,6));
-%     [b, fval] = Calib_Opt_Par(Yc, Yr, a, camera_intrinsic);
-%     ext = vertcat(ext,b);
-%     cost = vertcat(cost, fval);
-% end
-% 
-% [mincost,Idx] = min(cost);
-% opt_ext = ext(Idx,:);
-
 x0 = [0 0 pi/2 0 0 0 foc u0 v0];
-[ext, cost] = Calib_Opt_Fun_v2(Yc, Yr, x0);
+[ext, cost] = Calib_Opt_Fun_v2(Yc, Yr, x0, camera_intrinsic);
 
 for i = 1:50
-    [ext, cost] = Calib_Opt_Fun_v2(Yc, Yr, ext);
+    [ext, cost] = Calib_Opt_Fun_v2(Yc, Yr, ext, camera_intrinsic);
 end
 
 opt_ext = ext;
@@ -54,15 +40,6 @@ mincost = cost;
 
 
 %% find the spatial transformation matrices using the extrinsic parameters
-
-% t = trvec2tform(opt_ext(4:6));
-% r = rotm2tform(eul2rotm(opt_ext(1:3)));
-% 
-% Yrc = ci*t*r*Yr;
-% Yrc = Yrc./Yrc(3,:);
-% Yrc = ip*Yrc;
-% 
-% imgCalib_over(Yrc, imagefolder);
 
 t = trvec2tform(opt_ext(4:6));
 r = rotm2tform(eul2rotm(opt_ext(1:3)));
@@ -103,4 +80,9 @@ for i = 1:size(DataFiles)
 
 end
 
-%%
+%% Save Extrinsic Parameters
+
+ext_par = opt_ext(1:6);  %[yaw, pitch, roll, X, Y, Z]
+cam_int = opt_ext(7:9);  %[foc, u0, v0]
+
+save ext_par.mat ext_par;
